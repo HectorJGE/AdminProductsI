0c7ca43c22b42d213c54ce33a4a1db0c06f787cf
Hector <hector.gimenez.escurrau@gmail.com>
2023-01-27 20:19:16 -0300

    AdminProducts1

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import FormProducto from './components/FormProducto';
+
+function App() {
+  return (
+    <div>
+      <FormProducto/>
+    </div>
+  );
+}
+
+export default App;
